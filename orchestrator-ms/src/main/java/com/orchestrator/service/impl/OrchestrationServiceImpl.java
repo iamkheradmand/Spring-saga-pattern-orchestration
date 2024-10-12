2801588d9c9fb4be793f120b078986b370576c63
amirmohammadkheradmand <am.kheradmand@mydigipay.com>
2024-10-12 08:44:00 +0330

    chore:add new routing enum

--- orchestrator-ms/src/main/java/com/orchestrator/service/impl/OrchestrationServiceImpl.java
+++ orchestrator-ms/src/main/java/com/orchestrator/service/impl/OrchestrationServiceImpl.java
@@ -1,6 +1,6 @@
 package com.orchestrator.service.impl;
 
-import com.common.common.RabbitConstants;
+import com.common.common.RabbitMqSender;
 import com.common.dto.InventoryCheckResult;
 import com.common.dto.InventoryReverseResult;
 import com.common.dto.NotificationRequest;
@@ -8,63 +8,58 @@ import com.common.dto.OrderCancelResult;
 import com.common.dto.OrderCreatedRequest;
 import com.common.dto.PaymentResult;
 import com.common.enums.InventoryStatus;
+import com.common.enums.RabbitMqRouting;
 import com.common.enums.Status;
 import com.orchestrator.mapper.OrchestrationServiceMapper;
 import com.orchestrator.service.OrchestrationService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrchestrationServiceImpl implements OrchestrationService {
 
-	private final RabbitTemplate rabbitTemplate;
+	private final RabbitMqSender rabbitMqSender;
 
 	private final OrchestrationServiceMapper mapper;
 
 	@Override
 	public void orderCreatedEvent(OrderCreatedRequest createdRequest) {
-		rabbitTemplate.convertAndSend(RabbitConstants.inventoryExchange, RabbitConstants.inventoryCheckRouteKey,
-				mapper.toInventoryCheckRequest(createdRequest));
+		sendMessage(RabbitMqRouting.INVENTORY_CHECK, mapper.toInventoryCheckRequest(createdRequest));
 	}
 
 	@Override
 	public void orderCancelStatusEvent(OrderCancelResult cancelResult) {
-		rabbitTemplate.convertAndSend(RabbitConstants.notificationExchange, RabbitConstants.notificationRouteKey,
-				new NotificationRequest(cancelResult.getUserId(), "Your order canceled"));
+		sendMessage(RabbitMqRouting.NOTIFICATION, new NotificationRequest(cancelResult.getUserId(), "Your order canceled"));
 	}
 
 	@Override
 	public void inventoryCheckEvent(InventoryCheckResult checkResult) {
 		if (InventoryStatus.AVAILABLE.equals(checkResult.getInventoryStatus())) {
-			rabbitTemplate.convertAndSend(RabbitConstants.paymentExchange, RabbitConstants.paymentRouteKey,
-					mapper.toPaymentRequest(checkResult));
+			sendMessage(RabbitMqRouting.PAYMENT, mapper.toPaymentRequest(checkResult));
 		} else {
-			rabbitTemplate.convertAndSend(RabbitConstants.orderExchange, RabbitConstants.orderCancelRouteKey,
-					mapper.toOrderCancelRequest(checkResult));
+			sendMessage(RabbitMqRouting.ORDER_CANCEL, mapper.toOrderCancelRequest(checkResult));
 		}
 	}
 
 	@Override
 	public void inventoryReverseStatusEvent(InventoryReverseResult reverseResult) {
-		rabbitTemplate.convertAndSend(RabbitConstants.orderExchange, RabbitConstants.orderCancelRouteKey,
-				mapper.toOrderCancelRequest(reverseResult));
+		sendMessage(RabbitMqRouting.ORDER_CANCEL, mapper.toOrderCancelRequest(reverseResult));
 	}
 
 	@Override
 	public void paymentResultEvent(PaymentResult paymentResult) {
 		if (Status.SUCCESSFUL.equals(paymentResult.getStatus())) {
-			rabbitTemplate.convertAndSend(RabbitConstants.notificationExchange, RabbitConstants.notificationRouteKey,
-					new NotificationRequest(paymentResult.getUserId(), "Your order completed"));
-
-			rabbitTemplate.convertAndSend(RabbitConstants.orderExchange, RabbitConstants.orderCompleteRouteKey,
-					mapper.toOrderCompleteRequest(paymentResult));
+			sendMessage(RabbitMqRouting.NOTIFICATION, new NotificationRequest(paymentResult.getUserId(), "Your order completed"));
+			sendMessage(RabbitMqRouting.ORDER_COMPLETE, mapper.toOrderCompleteRequest(paymentResult));
 		} else {
-			rabbitTemplate.convertAndSend(RabbitConstants.inventoryExchange, RabbitConstants.inventoryReverseRouteKey,
-					mapper.toInventoryReverseRequest(paymentResult));
+			sendMessage(RabbitMqRouting.INVENTORY_REVERSE, mapper.toInventoryReverseRequest(paymentResult));
 		}
+	}
+
+	private void sendMessage(RabbitMqRouting routing, Object message) {
+		rabbitMqSender.send(routing, message);
 	}
 
 }
