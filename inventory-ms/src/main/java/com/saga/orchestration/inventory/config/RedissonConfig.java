b22df61e796ca9f823101d89060cfe6745eb88ba
amirmohammadkheradmand <am.kheradmand@mydigipay.com>
2024-10-16 13:37:13 +0330

    chore:read-Redisson-Config-from-yaml-file

--- inventory-ms/src/main/java/com/saga/orchestration/inventory/config/RedissonConfig.java
+++ inventory-ms/src/main/java/com/saga/orchestration/inventory/config/RedissonConfig.java
@@ -1,6 +1,7 @@
 package com.saga.orchestration.inventory.config;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
@@ -8,13 +9,17 @@ import org.redisson.config.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-//@Configuration
+@Configuration
 public class RedissonConfig {
-//
-//	@Bean(destroyMethod = "shutdown")
-//	public RedissonClient redissonClient() throws IOException {
-//		Config config = Config.fromYAML(getClass().getClassLoader().getResource("redisson/redisson.yaml"));
-//		return Redisson.create(config);
-//	}
+
+	@Bean(destroyMethod = "shutdown")
+	public RedissonClient redissonClient() throws IOException {
+		ClassPathResource resource = new ClassPathResource("redisson/redisson.yaml");
+		InputStream inputStream = resource.getInputStream();
+		Config config = Config.fromYAML(inputStream);
+		return Redisson.create(config);
+	}
+
 }
